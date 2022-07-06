f46823b599e433329ec681c5e6804740f1427ccd
DangPhuocTrieu.github.io <88046399+DangPhuocTrieu@users.noreply.github.com>
2022-07-06 22:24:14 +0700

    main

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { CardModule } from 'primeng/card';
 import { CarouselModule } from 'primeng/carousel';
+import { PaginatorModule } from 'primeng/paginator';
+import { PanelModule } from 'primeng/panel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './component/carousel/carousel.component';
+import { DetailProductComponent } from './component/detail-product/detail-product.component';
 import { HeaderComponent } from './component/header/header.component';
 import { HomeComponent } from './component/pages/home/home.component';
 import { PolicyComponent } from './component/policy/policy.component';
 import { ProductComponent } from './component/product/product.component';
 import { ProductsComponent } from './component/products/products.component';
-import { DetailProductComponent } from './component/detail-product/detail-product.component';
 
 
 @NgModule({
@@ -34,7 +37,10 @@ import { DetailProductComponent } from './component/detail-product/detail-produc
     ButtonModule,
     BadgeModule,
     CarouselModule,
-    HttpClientModule
+    HttpClientModule,
+    PaginatorModule,
+    PanelModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/component/detail-product/detail-product.component.ts
+++ client/src/app/component/detail-product/detail-product.component.ts
@@ -10,15 +10,37 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class DetailProductComponent implements OnInit {
   product!: Product
+  imageSelected!: string
+  sizes!: number[]
+  sizeSelected!: number
+
+
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getProduct()
+
+    this.sizes = [36, 37, 38, 39, 40, 41, 42]
+    this.sizeSelected = this.sizes[0]
   }
 
   getProduct() {
     let id = this.route.snapshot.paramMap.get('id')
-    this.productService.getProduct(id).subscribe(data => console.log(data))
+    this.productService.getProduct(id).subscribe(res => {
+      this.product = res.data as Product
+      this.imageSelected = this.product.images.image_1
+    })
   }
 
+  formatPrice(price: number) {
+    return this.productService.formatVND(price)
+  }
+
+  handleChangeImage(event: any) {
+    this.imageSelected = event.target.src
+  }
+
+  handleChooseSize(size: number) {
+    this.sizeSelected = size
+  }
 }
